6191303c70ca47250065a3485d02ee3ad33e0453
Galo DeSalazar <gdesalazar@gmail.com>
2020-02-28 20:34:27 -0800

    Add loaders to webpack

--- webpack.common.js
+++ webpack.common.js
@@ -20,46 +20,23 @@ module.exports = {
                     loader: "babel-loader",
                     options: { presets: ["env"] } 
                 }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
             }
-            // {
-            //     test: /\.css$/,
-            //     use: [
-            //         {
-            //             loader: MiniCssExtractPlugin.loader,
-            //             options: {
-                     
-            //                 publicPath: "../",
-            //                 hmr: process.env.NODE_ENV === "development"
-            //             }
-            //         },
-            //         "css-loader",
-            //         "postcss-loader"
-            //     ]
-            // },
-            // {
-            //     test: /\.scss/,
-            //     use: [
-            //         {
-            //             loader: MiniCssExtractPlugin.loader,
-            //             options: {
-            //                 // you can specify a publicPath here
-            //                 // by default it uses publicPath in webpackOptions.output
-            //                 publicPath: "../",
-            //                 hmr: process.env.NODE_ENV === "development"
-            //             }
-            //         },
-            //         "css-loader",
-            //         "sass-loader",
-            //         "postcss-loader"
-            //     ]
-            // }
         ]
-    }
-    // plugins: [new MiniCssExtractPlugin({
-    //     // Options similar to the same options in webpackOptions.output
-    //     // all options are optional
-    //     filename: "[name].css",
-    //     chunkFilename: "[id].css",
-    //     ignoreOrder: false // Enable to remove warnings about conflicting order
-    // }), require("autoprefixer")]
+    },
+    plugins: [
+        new MiniCssExtractPlugin()
+    ]
 };
