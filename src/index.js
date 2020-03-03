0fc5cf096fccb67a52ccd0e28fcdc229d660bc91
Galo DeSalazar <gdesalazar@gmail.com>
2020-03-03 15:54:17 -0800

    styled dropdown and changed fonts and sizes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './index.css'
+// import './mailchimp'
+
+console.log("hello")
+const signup = document.getElementById("mc_embed_signup");
+
+document.getElementById("yes").addEventListener("click", ()=>{
+    // signup.style.opacity = "1";
+    // signup.style.visibility = "visible"
+    signup.style.height = "100%";
+});
+document.getElementById("no").addEventListener("click", ()=>{
+    // signup.style.opacity = "1";
+    signup.style.height = "0";
+    // signup.style.visibility = "hidden"
+});
