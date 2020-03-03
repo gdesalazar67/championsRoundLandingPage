8a9679b3ca67ae99fc2a26505ee90d5277657a7c
Galo DeSalazar <gdesalazar@gmail.com>
2020-03-03 12:25:51 -0800

    urlloader4

--- webpack.common.js
+++ webpack.common.js
@@ -30,16 +30,16 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|gif)$/,
-                // use: [{
-                //     loader: 'url-loader',
-                //     options: {
-                //         limit: 8000, // Convert images < 8kb to base64 strings
-                //         name: 'images/[hash]-[name].[ext]'
-                //     }
-                // }]
-                use: [
-                    'file-loader'
-                ]
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        // limit: 8000, // Convert images < 8kb to base64 strings
+                        name: 'images/[hash]-[name].[ext]'
+                    }
+                }]
+                // use: [
+                //     'file-loader'
+                // ]
             }
         ]
     },
