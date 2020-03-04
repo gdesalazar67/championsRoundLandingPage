693b1cc1878ed672996a1a8c8e9106ea4430407d
Galo DeSalazar <gdesalazar@gmail.com>
2020-03-03 21:53:16 -0800

    fixed css

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ const signup = document.getElementById("mc_embed_signup");
 const thanks = document.getElementById("thanks");
 
 document.getElementById("yes").addEventListener("click", ()=>{
-    thanks.style.display = "block";
+    thanks.style.height = "30px";
+    thanks.style.opacity = "1"
+    thanks.style.marginBottom = "0px"
     signup.style.height = "100%";
+    signup.style.marginBottom = "25px";
 });
 document.getElementById("no").addEventListener("click", ()=>{
-    thanks.style.display = "block";
+    thanks.style.height = "30px";
+    thanks.style.opacity = "1"
     signup.style.height = "0";
+    thanks.style.marginBottom = "25px"
+    signup.style.marginBottom = "0px";
 });
