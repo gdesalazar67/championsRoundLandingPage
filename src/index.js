422cf59fbfb7f521a9bad3acba61415314bd2cf2
Galo DeSalazar <gdesalazar@gmail.com>
2020-03-05 12:18:49 -0800

    add resize for mobile

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import './index.css'
 
 const signup = document.getElementById("mc_embed_signup");
 const thanks = document.getElementById("thanks");
+const yes= document.getElementById("yes");
+let  h = window.innerHeight
 
+window.addEventListener("resize", () => {
+    h = window.innerHeight;
+});
+
+const scrollToDiv = (element) => {
+    if(h < 700){
+        window.scrollTo({
+            'behavior': 'smooth',
+            'left': 0,
+            'top': (element.offsetTop - 10)
+        });
+    };
+};
 
 document.getElementById("yes").addEventListener("click", ()=>{
     thanks.style.height = "30px";
@@ -10,6 +25,7 @@ document.getElementById("yes").addEventListener("click", ()=>{
     thanks.style.marginBottom = "0px"
     signup.style.height = "100%";
     signup.style.marginBottom = "25px";
+    scrollToDiv(yes);
 });
 document.getElementById("no").addEventListener("click", ()=>{
     thanks.style.height = "30px";
@@ -17,5 +33,6 @@ document.getElementById("no").addEventListener("click", ()=>{
     signup.style.height = "0";
     thanks.style.marginBottom = "25px"
     signup.style.marginBottom = "0px";
+    scrollToDiv(yes);
 });
 
